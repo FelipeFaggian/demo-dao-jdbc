9d619e65754ce13693141219c48076c5634c4808
faggian <felipesantosfaggian@gmail.com>
2023-05-10 20:55:21 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,12 @@
 package application;
 
-import java.sql.Connection;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = DB.getConnection();
-
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 	}
-
 }
